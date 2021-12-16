e5d1b8f3a0d8f4fa835a495b2d6e952baf382e2f
Joseph C Wang <joequant@gmail.com>
2021-12-16 09:12:47 +0800

    remove use2to3

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     long_description="""API's for cryptocurrency exchanges """,
     license="BSD",
     packages=['cryptoexchange', 'cryptoexchange/util'],
-    install_requires = ["websocket-client", "bitcoin-price-api"],
-    use_2to3 = True
+    install_requires = ["websocket-client", "bitcoin-price-api"]
 )
                                 
